432ce921c78bfdab8374028f1350b956e05a8b8c
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2021-07-01 18:00:41 +0000

    feat: add web interface

---
+++ web/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import csv
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get-status")
+def get_status():
+    try:
+        with open('../results.csv', newline='\n') as csvfile:
+            for row in reversed(list(csv.reader(csvfile))):
+                return {
+                    "timestamp": row[0],
+                    "total": int(row[1]) + int(row[2]),
+                    "up": row[1],
+                    "down": row[2]
+                }
+    except FileNotFoundError:
+        return {"timestamp": "--", "total": 0, "up": 0, "down": 0}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
